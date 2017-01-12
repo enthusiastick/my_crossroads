f658a3bffc05a8f1f3d71ca5bf9018c9289e4122
Eben <eben.lowe@gmail.com>
2017-01-12 18:43:39 -0500

    Initial commit.

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root "pages#index"
+
+  get "sign-in", to: "sessions#new", as: :sign_in
+  post "sign-in", to: "sessions#create"
+  delete "sign-out", to: "sessions#destroy"
+  get "sign-up", to: "users#new", as: :sign_up
+
+  resources :account_confirmations, only: [:edit]
+  resources :password_resets, only: [:create, :edit, :new, :update]
+  resources :users, only: [:create, :edit, :update]
+end
