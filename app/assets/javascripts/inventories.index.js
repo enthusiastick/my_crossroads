4859e6433fbc4bea904eeb932c822e6082a917ed
Eben <eben.lowe@gmail.com>
2017-01-19 17:57:36 -0500

    Fix bug when adding more than one new component to inventory.

--- app/assets/javascripts/inventories.index.js
+++ app/assets/javascripts/inventories.index.js
@@ -2,7 +2,9 @@ document.addEventListener("turbolinks:load", function() {
   function inventoryButtonClickHandler(e) {
     e.preventDefault();
     let formActions = document.getElementById('form-actions');
-    formActions.insertAdjacentHTML('beforebegin', inventoryButton.dataset.fields)
+    time = new Date().getTime()
+    regexp = new RegExp(inventoryButton.dataset.id, 'g')
+    formActions.insertAdjacentHTML('beforebegin', inventoryButton.dataset.fields.replace(regexp, time))
   }
   let inventoryButton = document.getElementById('add-component-button');
   if (inventoryButton) {
--- app/helpers/inventory_helper.rb
+++ app/helpers/inventory_helper.rb
@@ -1,9 +1,10 @@
 module InventoryHelper
   def button_to_add_inventory(f)
     new_inventory = f.object.send(:inventories).klass.new
-    fields = f.fields_for(:inventories, new_inventory) do |builder|
+    id = new_inventory.object_id
+    fields = f.fields_for(:inventories, new_inventory, child_index: id) do |builder|
       render partial: "ingredients_fields", locals: {form: builder}
     end
-    link_to("Add Component", "", class: "add_fields button", id: "add-component-button", data: {fields: fields.gsub("\n", "")})
+    link_to("Add Component", "", class: "add_fields button", id: "add-component-button", data: {id: id, fields: fields.gsub("\n", "")})
   end
 end
