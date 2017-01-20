396ce017379197f26ea5a30ca91aaa477720fc8c
Eben <eben.lowe@gmail.com>
2017-01-20 10:52:37 -0500

    Refactor to add inventory fields to top of form instead of bottom.

--- app/assets/javascripts/inventories.index.js
+++ app/assets/javascripts/inventories.index.js
@@ -1,10 +1,10 @@
 document.addEventListener("turbolinks:load", function() {
   function inventoryButtonClickHandler(e) {
     e.preventDefault();
-    let formActions = document.getElementById('form-actions');
+    let formInputs = document.getElementById('form-inputs');
     time = new Date().getTime()
     regexp = new RegExp(inventoryButton.dataset.id, 'g')
-    formActions.insertAdjacentHTML('beforebegin', inventoryButton.dataset.fields.replace(regexp, time))
+    formInputs.insertAdjacentHTML('beforebegin', inventoryButton.dataset.fields.replace(regexp, time))
   }
   let inventoryButton = document.getElementById('add-component-button');
   if (inventoryButton) {
