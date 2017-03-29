9012cb662984377d363d3d30121186253fba3e61
Eben <eben.lowe@gmail.com>
2017-03-29 11:36:23 -0400

    Update seedfile, include CSV data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,16 @@
-# print "Seeding users"
-# %w(staff foob barb bazb).each do |user|
-#   User.create(confirmed_at: Time.now, email: "#{user}@example.com", handle: user, first_name: user, last_name: user, password: "password")
-#   print "."
+require "csv"
+print " complete.\nSeeding seasons"
+{Ers: "January", Carris: "February", Tyir: "March", Rennse: "April", Fyrunn: "May", Medrim: "June", Arridun: "July", Grenn: "August", Ottur: "September", Curnnos: "October", Surrn: "November", Errund: "December", Qvellsvart: "Halloween", Varcindium: "Spring 2"}.each do |name, equivalent|
+  Season.find_or_create_by(name: name, equivalent: equivalent)
+  print "."
+end
+print " complete.\n"
+print "Seeding professions"
+  %w(Alchemy Apothecary Astrology Craftsman Herbalist/Mycologist Husbandry Merchant Miner/Geologist Ritualist Physician).each do |profession|
+    Profession.find_or_create_by(name: profession)
+    print "."
+  end
+print " complete.\n"
+# CSV.foreach("#{Rails.root}/db/data/seeds.csv", headers: true) do |row|
+#   binding.pry
 # end
-# User.find_by(handle: "staff").update(staff: true)
-# print " complete.\nSeeding characters"
-# %w(Leslie Ann Tom Ron Andy Ben Chris Donna Jerry).each do |character|
-#   user = User.order("RANDOM()").first
-#   Character.create(name: character, user: user, race: Character.races.keys.sample)
-#   print "."
-# end
-# print " complete.\nSeeding components"
-# %w(lorem ipsum dolor sit amet consectetur adipiscing elit).each do |component|
-#   Ingredient.create(name: component, category: Ingredient.categories.keys.sample, rarity: Ingredient.rarities.keys.sample)
-#   print "."
-# end
-# print " complete.\nSeeding seasons"
-# {Ers: "January", Carris: "February", Tyir: "March", Rennse: "April", Fyrunn: "May", Medrim: "June", Arridun: "July", Grenn: "August", Ottur: "September", Curnnos: "October", Surrn: "November", Errund: "December", Qvellsvart: "Halloween", Varcindium: "Spring 2"}.each do |name, equivalent|
-#   Season.create(name: name, equivalent: equivalent)
-#   print "."
-# end
-# print " complete.\n"
-# print "Seeding professions"
-#   %w(Alchemy Apothecary Astrology Craftsman Herbalist/Mycologist Husbandry Merchant Miner/Geologist Ritualist Physician).each do |profession|
-#     Profession.create(name: profession)
-#     print "."
-#   end
-# print " complete.\n"
