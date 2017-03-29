87c843895a97d34599aa64f0dc52ecc318a69255
Eben <eben.lowe@gmail.com>
2017-03-29 19:10:20 -0400

    Update seedfile.

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,6 +1,53 @@
 class Season < ApplicationRecord
+  MONTHS = [
+    "Ers",
+    "Carris",
+    "Tyir",
+    "Rennse",
+    "Fyrunn",
+    "Medrim",
+    "Arridun",
+    "Grenn",
+    "Ottur",
+    "Curnnos",
+    "Surrn",
+    "Errund"
+  ]
+  FALL = ["Ottur", "Curnnos", "Surrn"]
+  SPRING = ["Tyir", "Rennse", "Fyrunn"]
+  SUMMER = ["Medrim", "Arridun", "Grenn"]
+  WINTER = ["Errund", "Ers", "Carris"]
+
   has_many :ingredient_seasons
   has_many :ingredients, through: :ingredient_seasons
 
   validates_uniqueness_of :name
+
+  def self.months
+    where(name: MONTHS)
+  end
+
+  def self.fall
+    where(name: FALL)
+  end
+
+  def self.spring
+    where(name: SPRING)
+  end
+
+  def self.summer
+    where(name: SUMMER)
+  end
+
+  def self.winter
+    where(name: WINTER)
+  end
+
+  def self.quellsvart
+    find_by(name: "Qvellsvart")
+  end
+
+  def self.varcindium
+    find_by(name: "Varcindium")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,57 @@ print " complete.\nSeeding seasons"
   Season.find_or_create_by(name: name, equivalent: equivalent)
   print "."
 end
-print " complete.\n"
-print "Seeding professions"
+print " complete.\nSeeding professions"
   %w(Alchemy Apothecary Astrology Craftsman Herbalist/Mycologist Husbandry Merchant Miner/Geologist Ritualist Physician).each do |profession|
     Profession.find_or_create_by(name: profession)
     print "."
   end
-print " complete.\n"
-# CSV.foreach("#{Rails.root}/db/data/seeds.csv", headers: true) do |row|
-#   binding.pry
-# end
+print " complete.\nSeeding components"
+CSV.foreach("#{Rails.root}/db/data/seeds.csv", headers: true) do |row|
+  component = Ingredient.find_or_initialize_by(name: row["name"])
+  component.category = row["category2"].downcase.singularize
+  if row["rarity"].downcase == "ex. rare"
+    component.rarity = "extra_rare"
+  else
+    component.rarity = row["rarity"].downcase
+  end
+  case row["season"]
+  when "Any"
+    component.seasons = Season.months
+  when "Fall"
+    component.seasons = Season.fall
+  when "Spring"
+    component.seasons = Season.spring
+  when "Summer"
+    component.seasons = Season.summer
+  when "Spring/Summer"
+    component.seasons = Season.spring + Season.summer
+  when "Summer/Any"
+    component.seasons = Season.months
+  when "Spring/Fall"
+    component.seasons = Season.spring + Season.fall
+  when "Spring 2/Fall 1"
+    component.seasons = Season.where(name: ["Fyrunn", "Medrim", "Grenn", "Ottur"])
+    component.staff_notes = "Spring 2/Fall 1"
+  when "Spring 2, Fall 1"
+    component.seasons = Season.where(name: ["Fyrunn", "Medrim", "Grenn", "Ottur"])
+    component.staff_notes = "Spring 2/Fall 1"
+  when "Fall 2"
+    component.seasons = Season.where(name: ["Surrn", "Errund"])
+    component.staff_notes = "Fall 2"
+  when "Spring 1"
+    component.seasons = Season.where(name: ["Carris", "Tyir"])
+    component.staff_notes = "Spring 1"
+  when "Varcindium"
+    component.seasons << Season.varcindium
+  when "Quellsvart"
+    component.seasons << Season.quellsvart
+  end
+  component.level = row["level"]
+  component.process_extraction = row["extraction"]
+  component.process_calcination = row["calcination"]
+  component.process_dissolution = row["dissolution"]
+  component.process_concentration = row["concentration"]
+  print "." if component.save
+end
+print " complete."
