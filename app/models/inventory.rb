87c27ab32e64a4b3c64ebe01a194be2ec61a1bd4
Eben <eben.lowe@gmail.com>
2017-03-29 23:09:48 -0400

    Show/hide ingredient process data based on unlock attributes.

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,7 +2,7 @@ class Inventory < ApplicationRecord
   belongs_to :character
   belongs_to :ingredient
 
-  validates_inclusion_of :unlocked_calcination, :unlocked_concentration, :unlocked_dissolution, unlocked_extraction, in: [true, false]
+  validates_inclusion_of :unlocked_calcination, :unlocked_concentration, :unlocked_dissolution, :unlocked_extraction, in: [true, false]
   validates_numericality_of :quantity, greater_than_or_equal_to: 0, only_integer: true
   validates_presence_of :character, :ingredient, :quantity
   validates_uniqueness_of :ingredient, scope: :character
