6667be91e19c3b52431ecb4a8df66901f26eeeb5
Eben <eben.lowe@gmail.com>
2017-04-16 19:40:04 -0400

    Comment out seedfile.

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,31 +59,31 @@
 # end
 # print "\nComponent seeding complete."
 
-address = "1 Headquarters Rd Williamsburg, MA 01096"
-latitude = 42.452611
-longitude = -72.794706
-directions = "#### Directions to Camp Howe\n\n##### From East:\n\nI-90 West to I-91 North (Exit 4 on Mass. Pike). Take 91 North to Exit 19. Go STRAIGHT off of ramp (left and right turns are route 9) Follow Daimon/ Bridge Road aprox 5 miles (through several lights) until it comes to a stop sign in front of LOOK PARK Turn Right on Route 9 West. You will travel through a the town of Williamsburg and then into Goshen. In the center of Goshen (after you pass the Whale Inn), you will see a white church on the left and a school and church on the right. After the library, take the next right onto East Street. There is a large sign on the corner pointing to 4-H Camp Howe. Go half a mile to Goshen Police station and the DAR State Forest Headquarters (brown buildings on the left). Turn left behind the police station and follow the dirt road into camp.\n\n##### From North:\n\nTake I-91 south to Northampton (Routes 5-10-9). Take a right at the first set of lights. Follow to Route 9. Turn right on Route 9 heading West to Williamsburg. You will go through the town of Williamsburgh and then you will come into Goshen. It will be about15 miles from Northampton. In the center of Goshen (after you pass the Whale Inn), you will see a white church on the left and a school and church on the right. After the library, take the next right onto East Street. There is a large sign on the corner pointing to 4-H Camp Howe. Go half a mile to Goshen Police station and the DAR State Forest Headquarters (brown buildings on the left). Turn left behind the police station and follow the dirt road into camp.\n\n##### From South:\n\nTake I-91 North to exit 18 Northampton. Turn left on Route 9 heading West to Williamsburg. You will go through the towns of Florence, Williamsburgh and then you will come into Goshen. It will be about15 miles from Northampton. In the center of Goshen (after you pass the Whale Inn), you will see a white church on the left and a school and church on the right. After the library, take the next right onto East Street. There is a large sign on the corner pointing to 4-H Camp Howe. Go half a mile to Goshen Police station and the DAR State Forest Headquarters (brown buildings on the left). Turn left behind the police station and follow the dirt road into camp.\n\n##### From West:\n\nTake the Mass Pike to Pittsfield, MA. In Pittsfield, follow Route 9 East through Dalton. You will go through Windsor Mountain and Cummington. You will then come into the town of Goshen. After you pass the Fire station on your left you will see a left turn and a sign for 4-H Camp Howe across from the white church. Turn left. Go half a mile to Goshen Police station and the DAR State Forest Headquarters (brown buildings on the left). Turn left behind the police station and follow the dirt road into camp."
+# address = "1 Headquarters Rd Williamsburg, MA 01096"
+# latitude = 42.452611
+# longitude = -72.794706
+# directions = "#### Directions to Camp Howe\n\n##### From East:\n\nI-90 West to I-91 North (Exit 4 on Mass. Pike). Take 91 North to Exit 19. Go STRAIGHT off of ramp (left and right turns are route 9) Follow Daimon/ Bridge Road aprox 5 miles (through several lights) until it comes to a stop sign in front of LOOK PARK Turn Right on Route 9 West. You will travel through a the town of Williamsburg and then into Goshen. In the center of Goshen (after you pass the Whale Inn), you will see a white church on the left and a school and church on the right. After the library, take the next right onto East Street. There is a large sign on the corner pointing to 4-H Camp Howe. Go half a mile to Goshen Police station and the DAR State Forest Headquarters (brown buildings on the left). Turn left behind the police station and follow the dirt road into camp.\n\n##### From North:\n\nTake I-91 south to Northampton (Routes 5-10-9). Take a right at the first set of lights. Follow to Route 9. Turn right on Route 9 heading West to Williamsburg. You will go through the town of Williamsburgh and then you will come into Goshen. It will be about15 miles from Northampton. In the center of Goshen (after you pass the Whale Inn), you will see a white church on the left and a school and church on the right. After the library, take the next right onto East Street. There is a large sign on the corner pointing to 4-H Camp Howe. Go half a mile to Goshen Police station and the DAR State Forest Headquarters (brown buildings on the left). Turn left behind the police station and follow the dirt road into camp.\n\n##### From South:\n\nTake I-91 North to exit 18 Northampton. Turn left on Route 9 heading West to Williamsburg. You will go through the towns of Florence, Williamsburgh and then you will come into Goshen. It will be about15 miles from Northampton. In the center of Goshen (after you pass the Whale Inn), you will see a white church on the left and a school and church on the right. After the library, take the next right onto East Street. There is a large sign on the corner pointing to 4-H Camp Howe. Go half a mile to Goshen Police station and the DAR State Forest Headquarters (brown buildings on the left). Turn left behind the police station and follow the dirt road into camp.\n\n##### From West:\n\nTake the Mass Pike to Pittsfield, MA. In Pittsfield, follow Route 9 East through Dalton. You will go through Windsor Mountain and Cummington. You will then come into the town of Goshen. After you pass the Fire station on your left you will see a left turn and a sign for 4-H Camp Howe across from the white church. Turn left. Go half a mile to Goshen Police station and the DAR State Forest Headquarters (brown buildings on the left). Turn left behind the police station and follow the dirt road into camp."
 
-event1 = Event.find_or_initialize_by(name: "Spring One 2017")
-unless event1.persisted?
-  event1.assign_attributes(start_date: "2017-05-05 22:00:00 -0400", end_date: "2017-05-07 15:00:00 -0400", address: address, latitude: latitude, longitude: longitude, directions: directions)
-  print "#{event1.name}. " if event1.save
-end
+# event1 = Event.find_or_initialize_by(name: "Event One 2017")
+# unless event1.persisted?
+#   event1.assign_attributes(start_date: "2017-05-05 22:00:00 -0400", end_date: "2017-05-07 15:00:00 -0400", address: address, latitude: latitude, longitude: longitude, directions: directions)
+#   print "#{event1.name}. " if event1.save
+# end
 
-event2 = Event.find_or_initialize_by(name: "Spring Two 2017")
-unless event2.persisted?
-  event2.assign_attributes(start_date: "2017-06-02 22:00:00 -0400", end_date: "2017-06-04 15:00:00 -0400", address: address, latitude: latitude, longitude: longitude, directions: directions)
-  print "#{event2.name}. " if event2.save
-end
+# event2 = Event.find_or_initialize_by(name: "Event Two 2017")
+# unless event2.persisted?
+#   event2.assign_attributes(start_date: "2017-06-02 22:00:00 -0400", end_date: "2017-06-04 15:00:00 -0400", address: address, latitude: latitude, longitude: longitude, directions: directions)
+#   print "#{event2.name}. " if event2.save
+# end
 
-event3 = Event.find_or_initialize_by(name: "Fall One 2017")
-unless event3.persisted?
-  event3.assign_attributes(start_date: "2017-08-25 22:00:00 -0400", end_date: "2017-08-27 15:00:00 -0400", address: address, latitude: latitude, longitude: longitude, directions: directions)
-  print "#{event3.name}. " if event3.save
-end
+# event3 = Event.find_or_initialize_by(name: "Event Three 2017")
+# unless event3.persisted?
+#   event3.assign_attributes(start_date: "2017-08-25 22:00:00 -0400", end_date: "2017-08-27 15:00:00 -0400", address: address, latitude: latitude, longitude: longitude, directions: directions)
+#   print "#{event3.name}. " if event3.save
+# end
 
-event4 = Event.find_or_initialize_by(name: "Fall Two 2017")
-unless event4.persisted?
-  event4.assign_attributes(start_date: "2017-10-06 22:00:00 -0400", end_date: "2017-10-08 15:00:00 -0400", address: address, latitude: latitude, longitude: longitude, directions: directions)
-  print "#{event4.name}. " if event4.save
-end
+# event4 = Event.find_or_initialize_by(name: "Event Four 2017")
+# unless event4.persisted?
+#   event4.assign_attributes(start_date: "2017-10-06 22:00:00 -0400", end_date: "2017-10-08 15:00:00 -0400", address: address, latitude: latitude, longitude: longitude, directions: directions)
+#   print "#{event4.name}. " if event4.save
+# end
