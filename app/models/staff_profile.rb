de719c22c858aeb01c7889fa9cca8f094cd1a52a
Eben <eben.lowe@gmail.com>
2017-04-20 14:59:09 -0400

    Create migration and model.

---
+++ app/models/staff_profile.rb
@@ -0,0 +1,14 @@
+class StaffProfile < ApplicationRecord
+  belongs_to :user
+
+  validates_inclusion_of :published, in: [true, false]
+  validates_numericality_of :series, only_integer: true
+  validates_presence_of :title
+  validate :user_is_staff
+
+  def user_is_staff
+    unless user.staff?
+      errors.add(:user, "must be staff")
+    end
+  end
+end
